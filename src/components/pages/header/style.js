1c30be45a7cb53bdb236167e9155326ab9d13207
Yeong joon <bluehome8626@naver.com>
2021-11-18 22:44:48 +0900

    [CREATE] Header

---
+++ src/components/pages/header/style.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const Head = styled.div`
+  width: 100%;
+  height: 80px;
+  padding: 0;
+  background-color: #2e3c7e;
+`;
+export const HeadDiv = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const Input = styled.input`
+  width: 450px;
+  height: 30px;
+  margin-right: 130px;
+  border-radius: 5px;
+  border: none;
+  font-family: "Noto Sans KR", sans-serif;
+`;
+export const Button = styled.button`
+  margin-right: 40px;
+  border: none;
+  background-color: #2e3c7e;
+  color: white;
+  font-family: "Noto Sans KR", sans-serif;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const MarkImg = styled.img`
+  width: 70px;
+  height: 70px;
+  margin-top: 10px;
+  margin-right: 340px;
+  margin-left: 50px;
+`;
+
+export const DocImg = styled.img`
+  width: 18px;
+  height: 18px;
+  margin-left: 200px;
+  position: absolute;
+`;
