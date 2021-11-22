8a96328ccad4703183ff660af30b8a06cbb9576d
baejoonsoo <baejoonsoo21@gmail.com>
2021-11-22 23:43:14 +0900

    [UPDATE] size change

--- src/components/pages/showPost/ShowPost.jsx
+++ src/components/pages/showPost/ShowPost.jsx
@@ -1,20 +1,20 @@
-import * as S from './style';
-import patchBtn from '../../../assets/img/patchBtn.svg'
-import del from '../../../assets/img/del.svg'
+import * as S from "./style";
+import patchBtn from "../../../assets/img/patchBtn.svg";
+import del from "../../../assets/img/del.svg";
 
 const ShowPost = () => {
-  const text ={
+  const text = {
     // test용 객체
     writer: "kim",
-    title:"C++ 표준 라이브러리인 <algorithm> 라이브러리에 대해",
-    content:`
+    title: "C++ 표준 라이브러리인 <algorithm> 라이브러리에 대해",
+    content: `
     C++ 표준 라이브러리 <algorithm> 라이브러리에는 원소들에 대해 작업할 수 있는 여러가지 함수(검색, 정렬, 원소들 수정하기, 개수 세기 등등) 들을 정의하고 있습니다.
 
     이 때 작업할 원소들은 반복자 혹은 포인터를 통해 가리킬 수 있으며, 아래 함수들의 경우 작업을 시작할 원소와, 작업을 끝낼 원소 바로 뒤를 인자로 받게 됩니다. 식으로 표현하자면 [first, last) 의 형태로 보시면 됩니다.
     `,
-    language:"C/C++",
-  }
-    return (
+    language: "C/C++",
+  };
+  return (
     <S.Page>
       <S.Panel>
         <S.ContentHeader>
@@ -27,11 +27,11 @@ const ShowPost = () => {
         <S.Post>
           <S.BtnBar>
             <S.Button>
-              <img src={patchBtn} alt="patch button"/>
+              <img src={patchBtn} alt="patch button" />
               <label>수정</label>
             </S.Button>
             <S.Button>
-              <img src={del} alt="delete button"/>
+              <img src={del} alt="delete button" />
               <label>삭제</label>
             </S.Button>
           </S.BtnBar>
--- src/components/pages/showPost/style.js
+++ src/components/pages/showPost/style.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Page = styled.main`
   display: grid;
@@ -12,85 +12,85 @@ export const Panel = styled.div`
 `;
 
 export const Post = styled.section`
-  width: 1369px;
+  width: 1200px;
   /* height: 1607px; */
   height: auto;
   background-color: #e9ebf2;
   display: grid;
   justify-items: center;
-  border-radius:10px;
+  border-radius: 10px;
 `;
 
 export const PostText = styled.div`
-  width: 1150px;
+  width: 1000px;
   white-space: pre-line;
   word-break: break-all;
-  font-size:36px;
-  font-family:"Noto Sans kr";
-  margin:110px;
-  margin-top:50px;
-  color:#71737B;
-  font-weight:normal;
-  `;
-
-export const Title=styled.h1`
-  font-size:80px;
-  font-family:"Noto Sans kr";
-  color:#324290;
-  font-weight:normal;
+  font-size: 25px;
+  font-family: "Noto Sans kr";
+  margin: 100px;
+  margin-top: 50px;
+  color: #71737b;
+  font-weight: normal;
 `;
 
-export const Button=styled.div`
-  display:flex;
-  align-items:center;
+export const Title = styled.h1`
+  font-size: 60px;
+  font-family: "Noto Sans kr";
+  color: #324290;
+  font-weight: normal;
+`;
 
-  label{
-    margin-left:10px;
-    font-size:30px;
-    align-self:center;
+export const Button = styled.div`
+  display: flex;
+  align-items: center;
+
+  label {
+    margin-left: 5px;
+    font-size: 16px;
+    align-self: center;
   }
 `;
 
-export const BtnBar=styled.div`
-  display:grid;
-  grid-template-columns:repeat(2,1fr);
-  gap:22px;
-  margin:50px;
-  justify-self:right;
+export const BtnBar = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 22px;
+  margin: 50px;
+  justify-self: right;
 `;
 
-export const TopTitle=styled.span`
-  font-size:36px;
-  font-weight:bold;
-  color:#324290;
-  max-width:1000px;
-  display:block;
-  
-  overflow:hidden;
-  text-overflow:ellipsis;
-  white-space:nowrap;
+export const TopTitle = styled.span`
+  font-size: 24px;
+  font-weight: bold;
+  color: #324290;
+  max-width: 800px;
+
+  display: block;
+
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 `;
 
-export const Language=styled.span`
-  font-size:24px;
-  font-weight:bold;
-  margin-left:20px;
-  color:#324290;
-  padding-bottom: 5px;
+export const Language = styled.span`
+  font-size: 16px;
+  font-weight: bold;
+  margin-left: 10px;
+  color: #324290;
 `;
 
-export const TitleBar=styled.div`
+export const TitleBar = styled.div`
   display: flex;
-  align-items:flex-end;
+  align-items: flex-end;
 `;
 
-export const ContentHeader=styled.div`
-  display:flex;
+export const ContentHeader = styled.div`
+  display: flex;
   justify-content: space-between;
-  margin-bottom:30px;
+  margin-bottom: 20px;
 `;
 
-export const Writer=styled.span`
-  font-size:37px;
-  color:#324290;
+export const Writer = styled.span`
+  font-size: 26px;
+  color: #324290;
 `;
