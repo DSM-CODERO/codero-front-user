a5ec278ec6bdb4643fe5b4cd109632c0cd02d439
Yeong joon <bluehome8626@naver.com>
2021-11-25 20:26:47 +0900

    [UPDATE] ListPage
    복구

--- src/components/pages/listpage/ListPage.jsx
+++ src/components/pages/listpage/ListPage.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import pen from "../../../assets/pen.png";
 import * as s from "./style";
 
 function ListPage() {
-  return <div></div>;
+  return (
+    <s.List>
+      <s.ListHead>
+        <s.ListName>인기게시물</s.ListName>
+      </s.ListHead>
+      <s.ListBody>
+        <s.ListTable>
+          <thead>
+            <tr>
+              <s.ListTh>제목</s.ListTh>
+              <s.ListTh>분야</s.ListTh>
+              <s.ListTh>작성자</s.ListTh>
+              <s.ListTh>작성일</s.ListTh>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <s.ListTd>오류 해결하는 방법 알려주세요</s.ListTd>
+              <s.ListTd>JAVA</s.ListTd>
+              <s.ListTd>전영준</s.ListTd>
+              <s.ListTd>2021.11.09</s.ListTd>
+            </tr>
+          </tbody>
+        </s.ListTable>
+      </s.ListBody>
+      <s.BtnDiv>
+        <s.AddBtn type="button" src={pen} alt="logo" />
+      </s.BtnDiv>
+      <s.PageDiv></s.PageDiv>
+    </s.List>
+  );
 }
 export default ListPage;
--- src/components/pages/listpage/style.js
+++ src/components/pages/listpage/style.js
@@ -1,1 +1,64 @@
 import styled from "styled-components";
+
+export const List = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ListHead = styled.div`
+  width: 100%;
+  height: 30px;
+`;
+
+export const ListBody = styled.div`
+  width: 100%;
+  margin-top: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ListName = styled.h2`
+  color: #324290;
+  font-family: "Noto Sans KR", sans-serif;
+  margin-left: 270px;
+`;
+
+export const ListTable = styled.table`
+  width: 1000px;
+  border-top: 1px solid #444444;
+  border-collapse: collapse;
+`;
+
+export const ListTh = styled.th`
+  border-bottom: 1px solid #444444;
+  padding: 10px;
+`;
+
+export const ListTd = styled.td`
+  border-bottom: 1px solid #444444;
+  padding: 10px;
+  text-align: center;
+`;
+
+export const BtnDiv = styled.div`
+  width: 1270px;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const AddBtn = styled.input`
+  width: 80px;
+  height: 40px;
+  margin-top: 50px;
+  border: none;
+  border-radius: 10px;
+  background-color: #324290;
+`;
+
+export const PageDiv = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
