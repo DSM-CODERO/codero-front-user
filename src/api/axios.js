6ee30463e59b7b0039554a63bdafe28873da1b06
baejoonsoo <baejoonsoo21@gmail.com>
2021-12-14 20:27:34 +0900

    [ADD] axios

---
+++ src/api/axios.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+// const BASE_URL = `http://54.180.158.164`;
+axios.defaults.baseURL = 'http://54.180.158.164';
+
+export const requestWithToken = (method, url, headers, data) => {
+  console.log('request call');
+
+  return axios
+    .request({
+      method,
+      url,
+      headers: {
+        ...headers,
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+      },
+      data,
+    })
+    .then((res) => {
+      console.log('good');
+      return res.data;
+    })
+    .catch((err) => {
+      console.log(err);
+      throw new Error(err);
+    });
+};
